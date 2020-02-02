0a862c156ec58db59d3c4f3da2837a3e11c7217c
Pavel Beshchuk <PashaBeshchuk1990@gmail.com>
2020-02-02 12:20:20 +0300

    ollo

--- src/component/Header/Header.tsx
+++ src/component/Header/Header.tsx
@@ -1,11 +1,8 @@
 import React from "react"
 
 const Header:React.FC = () => {
-<<<<<<< HEAD
     let arr = [1,2,3,4,5,6]
-=======
-    let arr = [6, 5, 4, 3, 2, 1]
->>>>>>> second
+    let ollo = "dsfasdfsd"
     return <div>Header</div>
 }
 
