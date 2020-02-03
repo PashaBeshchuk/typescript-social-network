69cd57949fd61fd7b54da8b0478fcef951b987bd
Pavel Beshchuk <PashaBeshchuk1990@gmail.com>
2020-02-03 09:34:24 +0300

    pashaKrasavchik

--- src/component/Header/Header.tsx
+++ src/component/Header/Header.tsx
@@ -3,6 +3,7 @@ import React from "react"
 const Header:React.FC = () => {
     let arr = [1,2,3,4,5,6];
     let arr = [6, 5, 4, 3, 2, 1];
+    let Pasha = "Красавчик"
     return <div>Header</div>
 }
 
